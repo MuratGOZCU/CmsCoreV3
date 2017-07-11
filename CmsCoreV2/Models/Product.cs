7ec6760b6d55850de22a0200234b1c81ae8ce65e
Mustafafindik <mustafa.findik3@gmail.com>
2017-07-11 12:52:05 +0300

    product and product category model added

---
+++ CmsCoreV2/Models/Product.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CmsCoreV2.Models
+{
+    public class Product : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Slug { get; set; }
+        public string Description { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal SalePrice { get; set; }
+        public TaxStatus taxStatus { get; set; }
+        public TaxClass TaxClass { get; set; }
+        public string StockCode { get; set; }
+        public int StockCount { get; set; }
+        public bool StockStatus { get; set; }
+        public double weight { get; set; }
+        public double length { get; set; }
+        public double height { get; set; }
+        public double width { get; set; }
+        public ProductType ProductType { get; set; }
+        public string ProductUrl { get; set; }
+        public ICollection<Product> UpSells { get; set; }
+        public ICollection<Product> CrossSells { get; set; }
+        public ICollection<Product> GroupedProducts { get; set; }
+        public ICollection<ProducProductAttribute> ProductAttributes { get; set; }
+        public string purchaseNote { get; set; }
+        public int MenuOrder { get; set; }
+        public ICollection<ProductProductCategory> ProductCategories { get; set; }
+        public ICollection<ProductProductTag> ProductTags { get; set; }
+        public string ProductImage { get; set; }
+        public string ShortDescription { get; set; }
+        public ICollection<ProductMedia> ProductMedias { get; set; }
+        public int ViewCount { get; set; }
+        public int SaleCount { get; set; }
+        public CatalogVisibility CatalogVisibility { get; set; }
+        public bool IsFeatured { get; set; }
+
+    }
+}
---
+++ CmsCoreV2/Models/ProductProductCategory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CmsCoreV2.Models
+{
+    public class ProductProductCategory 
+    {
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+        public int ProductCategoryId { get; set; }
+        public ProductCategory ProductCategory { get; set; }
+
+    }
+}
