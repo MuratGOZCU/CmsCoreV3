90b633019d44b9d961faf4ef919ea357f13045af
ozlem06 <ozlm.yldrm06@gmail.com>
2017-07-14 12:05:59 +0300

    Coupon Added

---
+++ CmsCoreV2/Models/Coupon.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CmsCoreV2.Models
+{
+    public class Coupon
+    {
+        public string CouponCode { get; set; }
+        public string Description { get; set; }
+        public double DiscountRate { get; set; }
+        public decimal CouponBalance { get; set; }
+        public decimal MinimumSpending { get; set; }
+        public decimal MaximumSpending { get; set; }
+        public bool İndividual { get; set; }
+        public bool WithoutDiscountProduct { get; set; }
+        public virtual ICollection<Product> Products { get; set; }
+        public virtual ICollection<Product> WithoutProducts { get; set; }
+        public virtual ICollection<ProductCategory> ProductCategories { get; set; }
+        public virtual ICollection<String> RestictedEmail { get; set; }
+
+    }
+}
