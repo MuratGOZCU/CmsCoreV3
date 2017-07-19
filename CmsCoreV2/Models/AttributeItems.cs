68d6cfced8c91a2365417532831760661d61b15f
Onur Kapıcı <onurkapici@gmail.com>
2017-07-19 10:01:49 +0300

    AttributesProductTagAttributeItemsModelFixed

--- CmsCoreV2/Models/AttributeItems.cs
+++ CmsCoreV2/Models/AttributeItems.cs
@@ -12,15 +12,15 @@ namespace CmsCoreV2.Models
 
 
         [Display(Name = "Ad")]
-        [MaxLength(200)]
+        [StringLength(200)]
         public string Name { get; set; }
 
         [Display(Name = "Değer")]
-        [MaxLength(200)]
+        [StringLength(200)]
         public string Value { get; set; }
 
         [Display(Name = "Bağlantı")]
-        [MaxLength(200)]
+        [StringLength(200)]
         public string Slug { get; set; }
 
         [Display(Name = "Açıklama")]
