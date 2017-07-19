9d608c1808b708e4b2bf9d0d6356bc26a2bd3391
ömer örenç <orencomer@gmail.com>
2017-07-19 10:18:53 +0300

    SettingModulAdded

---
+++ CmsCoreV2/Models/EmailTemplate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CmsCoreV2.Models
+{
+    public class EmailTemplate:BaseEntity
+    {
+        [Display(Name = "Etkinleştir")]
+        public bool EnableThisEmailNotification { get; set; }
+        [Display(Name = "Alıcılar")]
+        public string Recipients { get; set; }
+        [StringLength(200)]
+        [Display(Name = "Konu")]
+        public string Subject { get; set; }
+        [StringLength(200)]
+        [Display(Name = "Email Başlığı")]
+        public string EmailTitle { get; set; }
+        [StringLength(200)]
+        [Display(Name = "Şablon")]
+        public string Template { get; set; }
+    }
+}
---
+++ CmsCoreV2/Models/TaxRate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CmsCoreV2.Models
+{
+    public class TaxRate:BaseEntity
+    {
+        [StringLength(200)]
+        [Display(Name ="Ülke Kodu")]
+        public string CountryCode { get; set; }
+        [Display(Name = "Şehir Plaka Kodu")]
+        public int CityPlateCode { get; set; }
+        [StringLength(200)]
+        [Display(Name = "Posta Kodu")]
+        public string ZipCode { get; set; }
+        [Display(Name = "Oran")]
+        public float Rate { get; set; }
+        [StringLength(200)]
+        [Display(Name = "Vergi Adı")]
+        public string TaxName { get; set; }
+        [Display(Name = "Öncelik")]
+        public int TaxRatePriority { get; set; }
+        [Display(Name = "Bileşik")]
+        public bool TaxRateCompound { get; set; }
+        [Display(Name = "Gönderim")]
+        public bool TaxRateShipping { get; set; }
+    }
+}
