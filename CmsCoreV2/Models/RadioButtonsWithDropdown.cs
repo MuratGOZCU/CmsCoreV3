6d7bd837bed6d94eba748f75011b97c413a193a9
Arzu Gedik <arzugedik312@gmail.com>
2017-07-28 12:47:00 +0300

    RadioButtonWithDropdownn added

---
+++ CmsCoreV2/Models/RadioButtonsWithDropdown.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CmsCoreV2.Models
+{
+    public class RadioButtonsWithDropdown
+    {
+        public IList<NameValueItem> Items { get; set; }
+    }
+}
