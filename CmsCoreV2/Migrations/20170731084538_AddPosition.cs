2a1f3b2061854daba4106cd5499933fb6c41ecfc
Arzu Gedik <arzugedik312@gmail.com>
2017-07-31 12:04:14 +0300

    Position added

---
+++ CmsCoreV2/Migrations/20170731084538_AddPosition.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CmsCoreV2.Migrations
+{
+    public partial class AddPosition : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Position",
+                table: "Posts",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Position",
+                table: "Posts");
+        }
+    }
+}
